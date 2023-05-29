024d4f0bd2f5f0f2a9cb6f92b372b4c310621eb3
Razal <razalbinkabeer01@gmail.com>
2023-05-29 12:58:42 +0400

    React Router implemented

---
+++ src/routes/Navbar/Navbar.components.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./Navbar.styles.scss";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <div>
+            <CrwnLogo />
+          </div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/shop"}>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navbar;
